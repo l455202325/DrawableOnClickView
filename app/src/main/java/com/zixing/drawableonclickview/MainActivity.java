3851c322afdcef4fef054fcb8424d27dba93d36b
l455202325 <liubaoping325@163.com>
2016-10-15 01:03:14 +0800

    增加EditText，版本升级版本到1.0.2,

--- app/src/main/java/com/zixing/drawableonclickview/MainActivity.java
+++ app/src/main/java/com/zixing/drawableonclickview/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-import com.zixing.drawableoncliickview.DrawableClickView;
+import com.zixing.drawableoncliickview.DrawableClickTextView;
 import com.zixing.drawableoncliickview.OnDrawableClickListener;
 import com.zixing.drawableoncliickview.OnDrawableItemClickListener;
 
@@ -14,51 +14,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DrawableClickView tv = (DrawableClickView) findViewById(R.id.drawableTextView);
+        DrawableClickTextView tv = (DrawableClickTextView) findViewById(R.id.drawableTextView);
 
         tv.setOnDrawableClickListener(new OnDrawableClickListener() {
             @Override
-            public void onLeftDrawableClick(DrawableClickView view) {
+            public void onLeftDrawableClick(DrawableClickTextView view) {
                 toastShow("我是全部设置的Left");
             }
 
             @Override
-            public void onTopDrawableClick(DrawableClickView view) {
+            public void onTopDrawableClick(DrawableClickTextView view) {
                 toastShow("我是全部设置的Top");
             }
 
             @Override
-            public void onRightDrawableClick(DrawableClickView view) {
+            public void onRightDrawableClick(DrawableClickTextView view) {
                 toastShow("我是全部设置的Right");
             }
 
             @Override
-            public void onBottomDrawableClick(DrawableClickView view) {
+            public void onBottomDrawableClick(DrawableClickTextView view) {
                 toastShow("我是全部设置的Bottom");
             }
         });
 
         tv.setLeftDrawableClickListener(new OnDrawableItemClickListener() {
             @Override
-            public void onDrawableClick(DrawableClickView view) {
+            public void onDrawableClick(DrawableClickTextView view) {
                 toastShow("我是分开设置的Left");
             }
         });
         tv.setTopDrawableClickListener(new OnDrawableItemClickListener() {
             @Override
-            public void onDrawableClick(DrawableClickView view) {
+            public void onDrawableClick(DrawableClickTextView view) {
                 toastShow("我是分开设置的Top");
             }
         });
         tv.setRightDrawableClickListener(new OnDrawableItemClickListener() {
             @Override
-            public void onDrawableClick(DrawableClickView view) {
+            public void onDrawableClick(DrawableClickTextView view) {
                 toastShow("我是分开设置的Right");
             }
         });
         tv.setButtomDrawableClickListener(new OnDrawableItemClickListener() {
             @Override
-            public void onDrawableClick(DrawableClickView view) {
+            public void onDrawableClick(DrawableClickTextView view) {
                 toastShow("我是分开设置的Buttom");
             }
         });
